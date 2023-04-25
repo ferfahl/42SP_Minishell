bf027b7c0d65a1467ac27dfb1f5e3d722e20070f
João Pedro <jpedrones@hotmail.com>
2023-04-25 15:10:58 -0300

    Add: ./mini_hell/ft_utils.c Arquivo para funções coringas usadas em escopos diferentes

---
+++ srcs/mini_hell/ft_utils.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joapedr2 < joapedr2@student.42sp.org.br    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/21 11:22:07 by joapedr2          #+#    #+#             */
+/*   Updated: 2023/04/25 01:31:39 by joapedr2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*ft_strjoin_free(char *s1, char *s2)
+{
+	char	*ret;
+
+	ret = ft_strjoin(s1, s2);
+	if (!ret)
+		terminate("ft_strjoin(): Failed");
+	free(s1);
+	return (ret);
+}
+
+int	ft_istrchr(const char *str, char c)
+{
+	int		i;
+	char	*find;
+
+	i = 0;
+	find = ft_strchr(str, c);
+	while ((str + i) && (str + i) != find)
+		i++;
+	return (i);
+}
