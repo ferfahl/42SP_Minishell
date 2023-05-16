b2529cfbbeb16815f1c26e4480df8f5533a55aba
João Pedro <jpedrones@hotmail.com>
2023-05-16 11:57:02 -0300

    [minishell] Update Reorganizei os diretórios e execuções para compor os checkers em um lugar específico

--- srcs/checkers/checker.c
+++ srcs/checkers/checker.c
@@ -1,39 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lexical_analyzer.c                                 :+:      :+:    :+:   */
+/*   checker.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joapedr2 < joapedr2@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/10 16:03:20 by joapedr2          #+#    #+#             */
-/*   Updated: 2023/05/16 11:54:42 by joapedr2         ###   ########.fr       */
+/*   Created: 2023/05/16 11:50:25 by joapedr2          #+#    #+#             */
+/*   Updated: 2023/05/16 11:55:14 by joapedr2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "tokenizer.h"
+#include "checkers.h"
 
-int	lexical_analyzer(void)
+int	run_all_checks(void)
 {
-	t_token	*aux;
-
-	make_tokens_list();
-	aux = g_data.tokens;
-	if (!aux)
+	if (!check_redir(g_data.input))
 		return (FALSE);
-	while (aux != NULL)
-	{
-		if (!syntax_analysis(aux))
-			return (FALSE);
-		aux = aux->next;
-	}
-	aux = g_data.tokens;
-	free(g_data.input);
-	g_data.input = ft_strdup("");
-	while (aux)
-	{
-		g_data.input = ft_strjoin_free(g_data.input, " ");
-		g_data.input = ft_strjoin_free(g_data.input, aux->cont);
-		aux = aux->next;
-	}
+	compress_quotes();
+	if (!create_cmd_list(g_data.input))
+		return (FALSE);
 	return (TRUE);
 }
